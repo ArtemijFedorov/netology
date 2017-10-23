b03ebcee018330faa9d99e6d28aed7104cae05b5
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2017-10-24 01:14:05 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,13 @@
+Fedorov Artemij!
+<?php
+  echo "<br />Имя:\nАртемий";
+  echo "<br />Фамилия:\nФёдоров";
+  echo PHP_EOL;
+  echo "Возраст: 26";
+  echo PHP_EOL;
+  echo "email: original_ti@mail.ru";
+  echo PHP_EOL;
+  echo "Город: Москва";
+  echo PHP_EOL;
+  echo "О себе: Системный администратор";
+ ?>
