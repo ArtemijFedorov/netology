ff1b7bc6d6e6e14c2e1d1b2b649a2e63bbe248fb
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2017-11-01 21:52:10 +0300

    Add files via upload

---
+++ Strings,arrays_and_objects.php
@@ -0,0 +1,25 @@
+<?php
+$countries = [
+    'Africa' => [
+        'camel'
+    ],
+    'Europa' => [
+        'Cow'
+    ],
+    'Asia' => [
+      'Panda'
+    ],
+    'South Pole' => [
+      'White Bear'
+    ],
+    'South America' => [
+      'Jaguar'
+    ],
+    'North America' => [
+      'Grizzly bear'
+    ],
+    'Australia' => [
+      'Kenkguru'
+    ]
+
+];
