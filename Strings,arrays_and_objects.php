1fea034d2e38e9f0ad99ebff6d6ea3a572e74083
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2017-11-19 14:37:29 +0300

    Update Strings,arrays_and_objects.php

--- Strings,arrays_and_objects.php
+++ Strings,arrays_and_objects.php
@@ -35,23 +35,32 @@ $continents = [
 foreach ($continents as $continent => $animals)//проходим по массиву $continents и создаем массив
     // $continent с ключами $animals
 {
+    //echo '<pre>';
+    //var_dump($animals);
+   // var_dump($continent);
+    //var_dump('Africa');
 
     foreach ($animals as $animal)//проходим по массиву $animals и даем значение $animal элементам
    {
-        $result = explode(" ", $animal);//название животных в строку
-        $animal_two = count($result);//разбиваем строку на слова и составляем массив
+        $arr_two[] = "$animal";
+        //$result = explode(" ", $animal);//название животных в строку
+       // $animal_two = count($result);//разбиваем строку на слова и составляем массив
         //$two = в это массиве должны быть массивы состоящие из двух ключей, какой воспользоваться функцией?
        //у меня есть предположение - через for, это верно?
 
-        echo '<pre>';
-       //print_r($animal_two);
-        //var_dump($continent);
-        var_dump($animal);
+        //echo '<pre>';
+        //var_dump($animal);
         //var_dump($animal_two);
         //var_dump($result);
+       //var_dump($arr_two);
 
        }
     }
+
+       echo '<pre>';
+       var_dump($arr_two);
+      // var_dump($result);
+
 
 
 
