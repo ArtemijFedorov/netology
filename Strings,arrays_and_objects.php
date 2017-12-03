4f41c4085e9ee1038c633fa121ca462e89bd1294
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2017-12-03 16:42:48 +0300

    Update Strings,arrays_and_objects.php

--- Strings,arrays_and_objects.php
+++ Strings,arrays_and_objects.php
@@ -32,24 +32,26 @@ $continents = [
 ];
 
 
-foreach ($continents as $continent => $animals)
-{
-    foreach ($animals as $animal)
-   {
-        $arr_two[] = $animal;
-       foreach ($arr_two as $item){
-           $result = explode(' ', $item);
-           $r_1[] = $result[0];
-           $r_2[] = $result[1];
+    foreach ($continents as $continent => $animals) {
 
-       }
-     }
 
+        foreach ($animals as $animal) {//название животных в строку
+            //echo '<pre>';
+            //var_dump($item);
+            $tmp = explode(' ', $animal);//разбиваем строку на слова и составляем массив
+            $a_1[] = $tmp[0];
+            $a_2[] = $tmp[1];
+            //echo '<pre>';
+            //var_dump($tmp);
+        }
     }
-
-       echo '<pre>';
-       //var_dump($arr_two);
-      //var_dump($r_2);
+    shuffle($a_1);
+    shuffle($a_2);
+    for ($i = 0, $j = count($a_1); $i < $j; $i++) {
+        $out[] = $a_1[$i] . ' ' . $a_2[$i];
+    }
+echo '<pre>';
+var_dump($out);
 
 
 
