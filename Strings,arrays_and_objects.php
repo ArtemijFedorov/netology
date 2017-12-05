3fa85163712b13eafea748e292158af0f473e3de
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2017-12-05 23:18:21 +0300

    Update Strings,arrays_and_objects.php

--- Strings,arrays_and_objects.php
+++ Strings,arrays_and_objects.php
@@ -27,31 +27,63 @@ $continents = [
 
     'Australia' => [
         'Kenkguru',
-        'Canis lupus dingo']
+        'Canis dingo']
 
 ];
 
+$name_two_words = [];
+foreach($continents as $continent => $animals){
+    foreach($animals as $animal){
+        $all_animals= [];
+        $anim = explode(' ', $animal);
+        $all_animals[]=$anim;
 
-    foreach ($continents as $continent => $animals) {
-
-
-        foreach ($animals as $animal) {//название животных в строку
-            //echo '<pre>';
-            //var_dump($item);
-            $tmp = explode(' ', $animal);//разбиваем строку на слова и составляем массив
-            $a_1[] = $tmp[0];
-            $a_2[] = $tmp[1];
-            //echo '<pre>';
-            //var_dump($tmp);
+        foreach($all_animals as $k){
+            if(count($k) === 2){
+                $comma_separated = implode(",", $k);
+                $str = str_replace(',', ' ', $comma_separated);
+                $name_two_words[]=$str;
+            }
         }
     }
-    shuffle($a_1);
-    shuffle($a_2);
-    for ($i = 0, $j = count($a_1); $i < $j; $i++) {
-        $out[] = $a_1[$i] . ' ' . $a_2[$i];
+}
+
+
+foreach($name_two_words as $name){
+    $parts = explode(' ', $name);
+    $first[] = $parts[0];
+    $second[] = $parts[1];
+}
+
+$random_first_word = [];
+
+while (count($random_first_word) < count($name_two_words)){
+    $proverka = $first[rand(0, count($name_two_words)-1)];
+    if (!in_array($proverka, $random_first_word)) {
+        array_push($random_first_word, $proverka);
     }
+}
+
+
+
+$random_second_word = [];
+
+while (count($random_second_word) < count($name_two_words)){
+    $proverka = $second[rand(0, count($name_two_words)-1)];
+    if (!in_array($proverka, $random_second_word)) {
+        array_push($random_second_word, $proverka);
+    }
+}
+
+
+
+$final_result = [];
+
+for($i = 0; $i < count($name_two_words); $i++){
+    $final_result[]= $random_first_word[$i] . ' ' . $random_second_word[$i];
+}
 echo '<pre>';
-var_dump($out);
+var_dump($final_result);
 
 
 
