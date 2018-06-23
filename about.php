1a215d940bdac918b2ef5682ffe5cc8276a2e0c5
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2018-06-24 00:10:14 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,20 +1,33 @@
-
 <?php
 
-  $userfirstName = "Артемий";
-  $usersecondName = "Фёдоров";
-  $userAge = "26 лет";
-  $userMail = "original_ti@mail.ru";
-  $userProf = "Сетевой инженер";
+$userfirstName = "Артемий";
+$usersecondName = "Фёдоров";
+$userAge = "26 лет";
+$userMail = "original_ti@mail.ru";
+$userProf = "Сетевой инженер";
 
- ?>
+?>
+<!DOCTYPE>
+<html lang="Ru-ru">
+<head>
+    <title><?= $userfirstName . ' – ' . $userProf ?></title>
+    <meta charset="utf-8">
+    <style>
+        body {
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+<h1>Страница пользователя <?= $userfirstName ?></h1>
+<div>
+    <h1 style="color: #e0be70;   text-decoration: overline;">Иноформация о пользователе</h1>
+    <p style="color: red; font-style: italic;">Имя: <strong><?= $userfirstName ?></strong></p>
+    <p style="color: blue; font-style: oblique;">Фамилия: <strong><?= $usersecondName ?></strong></p>
+    <p style="color:green; font-family: monospace;font-size: 18px">Возраст: <strong><?= $userAge ?></strong></p>
+    <p style="font-stretch: ultra-expanded; color: #61b9a9;text-transform: uppercase;">Mail: <strong><?= $userMail ?></strong></p>
+    <p style="color: #9995e1; text-transform: capitalize;">Профессия: <strong><?= $userProf ?></strong></p>
 
- <div>
-   <h1 style="color: #e0be70;   text-decoration: overline;">Иноформация о пользователе</h1>
-   <p style="color: red; font-style: italic;">Имя: <strong><?= $userfirstName ?></strong></p>
-   <p style="color: blue; font-style: oblique;">Фамилия: <strong><?= $usersecondName ?></strong></p>
-   <p style="color:green; font-family: monospace;font-size: 18px">Возраст: <strong><?= $userAge ?></strong></p>
-   <p style="font-stretch: ultra-expanded; color: #95e1d3;text-transform: uppercase;">Mail: <strong><?= $userMail ?></strong></p>
-   <p style="color: #9995e1; text-transform: capitalize;">Профессия: <strong><?= $userProf ?></strong></p>
-
- </div>
+</div>
+</body>
+</html>
