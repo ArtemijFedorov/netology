9254f46523e8eff9c2e296e4b61b11c787f712a7
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2018-08-24 16:07:20 +0300

    Update homePHP.php

--- homePHP/homePHP.php
+++ homePHP/homePHP.php
@@ -1,18 +1,19 @@
 <?php
-    $x = rand(0, 100);
-    $a = 1;
-    $b = 1;
-    $c = 0;
+    $userNumber = rand(0, 100);
+    $numderA = 1;
+    $numberB = 1;
+    $numberC = 0;
     do {
-        if ($a > $x) {
+        if ($numderA > $userNumber) {
             echo 'Задуманное число НЕ входит в числовой ряд';
             break;
-        } elseif ($a == $x) {
+        } elseif ($numderA == $userNumber) {
             echo 'Задуманное число входит в числовой ряд';
+            echo 'Чило ' . "$userNumber" 
             break;
         } else {
-            $c = $a;
-            $a= $a + $b;
-            $b = $c;
+            $numberC = $numderA;
+            $numderA= $numderA + $numberB;
+            $numberB = $numberC;
         }
     } while (true);
