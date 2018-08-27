0536fcbc4cab723cfc52f0d3c934a42cc2092b0a
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2018-08-28 01:26:32 +0300

    Update newArray.php

--- arrayFiles/newArray.php
+++ arrayFiles/newArray.php
@@ -35,21 +35,23 @@ $two_animals = [];
 foreach($continents as $continent => $animals) {
     foreach ($animals as $animal) {
         $words = (explode(' ', $animal));
-       // $two_animals = [];
+
         if (count($words) == 2) {
             $two_animals[] = $words;
         }
     }
 }
 echo '<pre>';
-var_dump($two_animals);
+//var_dump($two_animals);
 //var_dump(count($two_animals));
 
 foreach($two_animals as $two_word) {
-    $word = explode(' ', $two_word);
-    $first_name[] = $word[0];
-    $second_name[] = $word[1];
+    //var_dump($two_word);//
+    // $word = explode(' ', $two_word);
+    $first_name[] = $two_word[0];//$word[0];
+    $second_name[] = $two_word[1]; //$word[1];
 }
+
 //$first_name = array_map(function ($a){return explode(' ', $a)[0];}, $two_animals);
 //$second_name = array_map(function ($a){return explode(' ', $a)[1];}, $two_animals);
 
